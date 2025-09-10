be77e22bc7224edd197fe7cb46916f3d84f07c1a
Roger <rogerr_machadoo@hotmail.com>
2025-09-09 21:03:47 -0300

    Implementação do exercicio restante.

--- Exe_03/Program.cs
+++ Exe_03/Program.cs
@@ -18,6 +18,22 @@ namespace Course {
             p.Quantidade = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Dados do Produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
         }
     }
 }
